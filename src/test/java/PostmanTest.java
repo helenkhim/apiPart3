dcd724628252180af66d51ed0c0273e5edb9bda4
Elena <elena.bogacheva97@gmail.com>
2021-05-29 16:50:31 +0300

    test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -1,2 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class PostmanTest {
+    @Test
+    void shouldReturnStatus200And() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+
+        ;
+    }
 }
+
